600055291ab64144fa9154cad1ee1295c4cee955
Theo Crithary <theo@apacit.com.au>
2020-04-17 10:21:50 +1000

    .

--- Day 2/Lab 03 - Part 2 - Deploying to Kubernetes/app.py
+++ Day 2/Lab 03 - Part 2 - Deploying to Kubernetes/app.py
@@ -31,6 +31,6 @@ def upload():
 def photo(photo):
     return render_template('photo.html',photo=photo)
 
-##### Run the Flask instance, browse to http://<< Host IP or URL >>:5000 #####
+##### Run the Flask instance, browse to http://<< Host IP or URL >>:5003 #####
 if __name__ == "__main__":
-	app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5000')), threaded=True)
+	app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5003')), threaded=True)
